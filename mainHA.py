7a360ec4c4f51fc5e500ced8ceda11120dfceb71
Ahmet <56405912+bicerahmet@users.noreply.github.com>
2022-05-19 23:18:57 +0300

    Update mainHA.py

--- mainHA.py
+++ mainHA.py
@@ -2,6 +2,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from pyqtUI import Ui_MainWindow
 import sys
 
+
 app = QtWidgets.QApplication(sys.argv)
 MainWindow = QtWidgets.QMainWindow()
 ui = Ui_MainWindow()
